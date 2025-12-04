f0d56e095b18c54d2ff5e848f8bcd9873374277b
Vagner Jorge Souza Feitosa <svagnerjorge@gmail.com>
2025-12-03 22:53:53 -0300

    Revert "Add Vercel Speed Insights integration"
    This reverts commit 4af29453c21ca062fa0db2c6451b835ac8de4e75.

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import cors from "cors";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 
 dotenv.config();
